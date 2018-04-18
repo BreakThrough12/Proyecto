099cd6bdf62444bc8a75d224bad985831874f687
Christofer <cris_sierra12@hotmail.com>
2018-04-18 14:20:51 -0300

    Primer Commit

---
+++ Prueba.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	float altura;
+	int peso;
+	double IMC;
+	printf("Ingrese su altura en metros\n");
+	scanf("%f",&altura);
+	printf("Ingrese su peso\n");
+	scanf("%d",&peso);
+
+	
+	return 0;
+}
